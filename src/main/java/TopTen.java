c71326f5ceae819c3a33aeb90efd0755c814f323
senkaavdagic <senka.music@gmail.com>
2017-07-12 14:15:59 +0200

    Add files via upload

--- src/main/java/TopTen.java
+++ src/main/java/TopTen.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -97,8 +94,9 @@ public class TopTen  implements TopTenInterface {
     } */
 
 
-    public List<String> parseText(String text, List<Character> delimiters){ //hashmap ili nesto samo za lookup!
+    public List<String> parseText(String text, List<Character> delimiters){ //hashmap ili nesto samo za lookup! ok!
 
+        String everything=null;
         List<String> res = new ArrayList<String>();
         HashMap<Integer, Character> delimiteri = new HashMap <Integer, Character> ();
         for (int i=0; i<delimiters.size(); i++)
@@ -106,16 +104,48 @@ public class TopTen  implements TopTenInterface {
                 delimiteri.put(i,delimiters.get(i) );
             }
 
+        // blacklist iz filea
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("blacklist.txt"));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            everything = sb.toString();
+            br.close();
+        } catch (Exception e) {
+            System.out.println (e.getMessage());
+        }
+
+        HashMap<Integer, String> blacklista = new HashMap<>();
+
+        String [] rijeci = everything.split(System.lineSeparator());
+        for (int i=0; i<rijeci.length; i++)
+        {
+            blacklista.put(i, rijeci[i].toLowerCase());
+        }
+
+        System.out.println ("\n blacklista \n");
+        for (Integer key : blacklista.keySet()) {
+            System.out.println(key + "\t\t" + blacklista.get(key));
+        }
+
         int d=0; //position of last delimiter
         for(int i=0; i < text.length(); i++){
             char c = text.charAt(i);
             if(delimiteri.containsValue(c)) {
                 String word = text.substring(d,i);
-                if(word.length() > 0) res.add(word);
+                if((word.length() > 2)&&(!blacklista.containsValue(word))) res.add(word.toLowerCase());
                 d = i+1;
             }
         }
 
+        for (int i=0; i<res.size(); i++) {System.out.println((i+1) + ". " + res.get(i));}
         return res;
     }
 
